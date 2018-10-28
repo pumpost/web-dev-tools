0c0039a9b0f7bf7f5758df7681f80d66911d3e32
oh <w.juthatip@gmail.com>
2018-10-29 00:44:15 +0700

    update

--- src/containers/RandomObject/InputObj.js
+++ src/containers/RandomObject/InputObj.js
@@ -2,21 +2,26 @@ import React, { Component, Fragment } from "react";
 import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
 
 class InputObj extends Component {
-  state = {
-    objectVal: ""
-  };
-
-  updateInputValue = (input, evt) => {
-    this.setState({ [input]: evt.target.value });
-  };
-
   render() {
+    const { result } = this.props;
+
     return (
       <Fragment>
         <Input
-          value={this.state.objectVal}
-          onChange={this.updateInputValue.bind(this, "objectVal")}
+          value={this.props.value}
+          onChange={e => this.props.handleUpdateVal(e, "objectVal")}
         />
+        <FormGroup check>
+          <Label check>
+            <Input
+              type="checkbox"
+              checked={this.props.numeric}
+              onChange={this.props.handleCheckbox}
+              name="numeric"
+            />{" "}
+            Numeric digits (0-9)
+          </Label>
+        </FormGroup>
       </Fragment>
     );
   }
--- src/containers/RandomObject/RandomObject.js
+++ src/containers/RandomObject/RandomObject.js
@@ -3,28 +3,101 @@ import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
 import InputObj from "./InputObj";
 
 class RandomObject extends Component {
-  // state = {
-  //   objectVal: ""
-  // };
-
-  // updateInputValue = (input, evt) => {
-  //   this.setState({ [input]: evt.target.value });
-  // };
-
-  addButton = () => {
-    console.log(111);
+  state = {
+    objectVal: "",
+    numeric: true,
+    result: [{ text: "111" }, { text: "2222" }]
   };
 
+  updateInputValue = (evt, input) => {
+    this.setState({ [input]: evt.target.value });
+  };
+
+  handleCheckbox = evt => {
+    const target = evt.target;
+    this.setState({ [target.name]: target.checked });
+  };
+
+  randomAll() {
+    let number = "0123456789";
+    for (var i = 0; i < 5; i++) {
+      return number.charAt(Math.floor(Math.random() * number.length));
+    }
+  }
+
+  addButton = () => {
+    const randomResult = this.randomAll();
+    const objName = this.state.objectVal;
+    let result = [];
+    result.push({ [objName]: randomResult });
+    this.setState({ result });
+  };
+
+  generateResult = () => {
+    // console.log(111);
+    // console.log(result);
+  };
+
+  renderInput() {
+    return this.state.result.map((data, i) => {
+      return (
+        <InputObj
+          key={i}
+          handleUpdateVal={this.updateInputValue}
+          value={data.text}
+          numeric={this.state.numeric}
+          handleCheckbox={this.handleCheckbox}
+        />
+      );
+    });
+  }
+
   render() {
-    // console.log(this.state.objectVal);
+    const { result } = this.state;
+
+    console.log(result);
     return (
       <div className="text-left container">
         <h2> Random Object Generator</h2>
         <FormGroup>
           <Label for="exampleEmail">Random Object</Label>
           Generate
-          <InputObj />
-          <Button onClick={this.addButton}>+</Button>
+          {/* <InputObj
+            handleUpdateVal={this.updateInputValue}
+            value={this.state.objectVal}
+            numeric={this.state.numeric}
+            handleCheckbox={this.handleCheckbox}
+          /> */}
+          {/* {result.length > 0 ? (
+            this.renderInput()
+          ) : (
+            <InputObj
+              handleUpdateVal={this.updateInputValue}
+              value={this.state.objectVal}
+              numeric={this.state.numeric}
+              handleCheckbox={this.handleCheckbox}
+            />
+          )} */}
+          {result.map((data, i) => {
+            return (
+              <InputObj
+                key={i}
+                handleUpdateVal={this.updateInputValue}
+                value={data.text}
+                numeric={this.state.numeric}
+                handleCheckbox={this.handleCheckbox}
+              />
+            );
+          })}
+          <Button onClick={this.addButton} color="success">
+            +
+          </Button>
+          <div>
+            {" "}
+            <Button color="primary" onClick={this.generateResult}>
+              Generate
+            </Button>
+          </div>
         </FormGroup>
       </div>
     );
